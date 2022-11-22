1075097840b5239c3815ed0d034aa28ec2f6546b
jkhbuild <jkh.build@gmail.com>
2022-11-22 15:47:32 -0500

    greenhouse nigth and day images

--- greenHouse/src/components/Greenhouse/Greenhouse.js
+++ greenHouse/src/components/Greenhouse/Greenhouse.js
@@ -1,22 +1,31 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
+import { useTheme } from "../../context/ThemeContext.js";
 
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
 
 function Greenhouse() {
+  const { themeName, setThemeName } = useTheme();
 
-  return (
-    <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
-      <LightSwitch />
-      <ClimateStats />
-    </section>
-  );
+  if (themeName === "day") {
+    return (
+      <section>
+        <img className="greenhouse-img" src={dayImage} alt="greenhouse" />
+        <LightSwitch />
+        <ClimateStats />
+      </section>
+    );
+  } else {
+    return (
+      <section>
+        <img className="greenhouse-img" src={nightImage} alt="greenhouse" />
+        <LightSwitch />
+        <ClimateStats />
+      </section>
+    );
+  }
 }
 
 export default Greenhouse;
--- greenHouse/src/context/ThemeContext.js
+++ greenHouse/src/context/ThemeContext.js
@@ -1,17 +1,17 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export const useTheme = () => useContext(ThemeContext);
 
 export default function ThemeProvider({ children }) {
-  const [themeName, setThemeName] = useState("day");
+  const [themeName, setThemeName] = useState("night");
 
   return (
     <ThemeContext.Provider
       value={{
         themeName,
-        setThemeName
+        setThemeName,
       }}
     >
       {children}
